24cd6318c84a5d92c790fb5a7c4a5b4da9977b67
Summer Rizzo <srainrizzo@gmail.com>
2020-04-07 13:29:44 -0400

    update sizing to rel units

--- src/Components/about-me.jsx
+++ src/Components/about-me.jsx
@@ -6,8 +6,8 @@ import self from '../static/self.JPG';
 export default function AboutMe (props) {
     return(
         <div className={styles.container}>
-            <h1>Hello!</h1>
-            <h2>My name is Summer Rain Rizzo.</h2>
+            <h1 className={styles.header}>Hello!</h1>
+            <h2 className={styles.subhead}>My name is Summer Rain Rizzo.</h2>
             <img src={self} className={styles['self-img']} alt="a picture of myself that is totally not a selfie"></img>
             <div className={styles.content}>
             <p>I've always had an affinity for creativity. Seeing the visions that float around my mind come to life is pure magic. I've worn the hat of a filmmaker, photographer, radio DJ, barista and most recently, a developer. Although the context & execution varies, my drive for each of them comes from the same place in my heart - to make things that make a difference. <span className={styles.noMatter}>(no matter how small)</span> </p>
