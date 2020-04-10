eac02ef5ad932d6045ba69128b014bc9fc0f3c23
Summer Rizzo <srainrizzo@gmail.com>
2020-04-10 00:49:16 -0400

    tweak about me, fix mobile pdf view

--- src/Components/about-me.jsx
+++ src/Components/about-me.jsx
@@ -11,7 +11,7 @@ export default function AboutMe (props) {
             <img src={self} className={styles['self-img']} alt="a picture of myself that is totally not a selfie"></img>
             <div className={styles.content}>
             <p>I've always had an affinity for creativity. Seeing the visions that float around my mind come to life is pure magic. I've worn the hat of a filmmaker, photographer, radio DJ, barista and most recently, a developer. Although the context & execution varies, my drive for each of them comes from the same place in my heart - to make things that make a difference. <span className={styles.noMatter}>(no matter how small)</span> </p>
-            <p>I'm a firm advocate of diversity, inclusion, and accessibility. A few other interests of mine include music, video games, eating apples, and enthusing about my interests.  And, if I know and like you well enough, I'm known to mix a pretty good cocktail.</p>
+            <p>I'm a firm advocate of diversity, inclusion, and accessibility. A few other interests of mine include listening to music, film photography, making various things from scratch, eating apples, and enthusing about my interests.  And, if I know and like you well enough, I'm known to mix a pretty good cocktail.</p>
             <p>I'll leave the rest of getting to know me to you.</p>
             <p>♡</p>
             </div>
--- src/Components/resume.jsx
+++ src/Components/resume.jsx
@@ -1,14 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './resume.modules.css';
 
 import resume from '../static/resume.pdf'
 
 export default function Resume () {
 
+    // returns true if device view width is less than or equal to 768px
+    let mobileViewWidth = window.matchMedia('(max-width: 768px)').matches;
+
     return (
         <div className={styles.content}>
-            <iframe src={resume} className={styles['resume-pdf']}/>
-            {/* <object data={resume} className={styles['resume-pdf']}/> */}
+            <object data={ mobileViewWidth ? resume + '?#zoom=50' : resume + '?#zoom=90'} className={styles['resume-pdf']}/>
         </div>
     )
 }
